c63b1961a770a9d2ef4dce3f4b5f6fa04c45553e
Ivo Goncalves <ivojmgs19@gmail.com>
2023-04-28 18:55:38 -0300

    lazyLoad for components

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+//Modules
+import { RoutingModule } from './routing.module';
 //Components pages
 import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RoutingModule
   ]
 })
 export class PagesModule { }
