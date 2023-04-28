b71c7617e9d036624b85af79afa96f43e9ee657e
Ivo Goncalves <ivojmgs19@gmail.com>
2023-04-28 19:03:41 -0300

    Created route for Component Details

--- src/app/pages/routing.module.ts
+++ src/app/pages/routing.module.ts
@@ -3,10 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 //Components
 import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   {
     path: '', component: HomeComponent
+  },
+  {
+    path: 'details', component: DetailsComponent
   }
 ];
 
