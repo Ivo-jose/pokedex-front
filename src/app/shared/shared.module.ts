db7a832ac1090205d0cf38a46c30af25038ba6d4
Ivo Goncalves <ivojmgs19@gmail.com>
2023-05-05 15:45:48 -0300

    Created Poke-list Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,13 +6,15 @@ import { PagesModule } from '../pages/pages.module';
 //Component
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 
 
 @NgModule({
   declarations: [
     PokeHeaderComponent,
-    PokeSearchComponent
+    PokeSearchComponent,
+    PokeListComponent
   ],
   exports: [
     PokeHeaderComponent,
